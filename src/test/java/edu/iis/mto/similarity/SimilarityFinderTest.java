a692e6c7994fb7bd6b2fe985e0fb5e77149c09f8
Paxon96 <bwloczyk@gmail.com>
2019-03-31 12:32:22 +0200

    add test cases

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -0,0 +1,40 @@
+package edu.iis.mto.similarity;
+
+import org.junit.Test;
+
+public class SimilarityFinderTest {
+
+    @Test
+    public void jackardSimilarityWithEmptySequencesTest() {
+    }
+
+    @Test
+    public void jackardSimilarityWithEmptySequenceTest() {
+    }
+
+    @Test
+    public void jackardSimilaritySequencesDifferentSizesWithCommonElementTest() {
+    }
+
+    @Test
+    public void jackardSimilaritySequencesDifferentSizesWithoutCommonElementTest() {
+    }
+
+    @Test
+    public void jackardSimilaritySequencesSameSizeWithCommonElementTest() {
+    }
+
+    @Test
+    public void jackardSimilaritySequencesSameSizeWithoutCommonElementTest() {
+    }
+
+    @Test
+    public void jackardSimilaritySequencesDifferentSizesWithPartCommonElementTest() {
+    }
+
+    @Test
+    public void jackardSimilaritySequencesDifferentSizesWithAllCommonElementTest() {
+    }
+
+
+}
