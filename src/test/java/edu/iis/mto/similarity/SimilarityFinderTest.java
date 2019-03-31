8af8c089220735512de64c88c10a1a4410465a77
Paxon96 <bwloczyk@gmail.com>
2019-03-31 13:06:32 +0200

    add tests with empty sequences

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,15 +1,38 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.similarity.dubler.SequenceSearcherDubler;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 public class SimilarityFinderTest {
 
+    private SimilarityFinder finder;
+    private final double SAME_SEQUENCES = 1.0;
+    private final double DIFFERENT_SEQUENCES = 0.0;
+
+    @Before
+    public void init(){
+        finder = new SimilarityFinder(new SequenceSearcherDubler());
+    }
+
     @Test
     public void jackardSimilarityWithEmptySequencesTest() {
+        int[] firstSequence = {};
+        int[] secondSequence = {};
+
+        Assert.assertThat(SAME_SEQUENCES,is(equalTo(finder.calculateJackardSimilarity(firstSequence,secondSequence))) );
     }
 
     @Test
     public void jackardSimilarityWithEmptySequenceTest() {
+        int[] firstSequence = {1,2,3,4,5};
+        int[] secondSequence = {};
+
+        Assert.assertThat(DIFFERENT_SEQUENCES,is(equalTo(finder.calculateJackardSimilarity(firstSequence,secondSequence))) );
     }
 
     @Test
